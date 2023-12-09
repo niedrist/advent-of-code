4be18d9bfe525ca5d8f184f808ea66be77497840
alexanderniedrist <Alexander.Niedrist@rtl-extern.de>
2023-12-09 13:35:19 +0100

    reduce code in day 09

--- src/main/kotlin/y2023/day09/Day09.kt
+++ src/main/kotlin/y2023/day09/Day09.kt
@@ -7,14 +7,12 @@ val d = FileReader.asStrings("2023/day09.txt").map { it.split(" ").map { n -> n.
 fun main() = Day09.run()
 
 object Day09 : BasicDay() {
-    override fun part1() = d.sumOf { it.differenceSteps().extrapolated() }
+    override fun part1() = d.sumOf { it.extrapolated() }
 
-    override fun part2() = d.sumOf { it.reversed().differenceSteps().extrapolated() }
+    override fun part2() = d.sumOf { it.reversed().extrapolated() }
 }
 
-fun List<Int>.differenceSteps() =
+fun List<Int>.extrapolated() =
     generateSequence(this) { current ->
         current.zipWithNext { a, b -> b - a }.takeIf { !it.all { x -> x == 0 } }
-    }.toList()
-
-fun List<List<Int>>.extrapolated() = this.sumOf { it.last() }
+    }.toList().sumOf { it.last() }
